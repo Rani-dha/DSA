31080e139f7b76343edd991f29cd08965d05ce18
Dharani T <gct.it18.dharani@gmail.com>
2021-04-01 23:46:01 +0530

    Day 1 Duplicate number

---
+++ 1 Arrays/Day 1/1 Find the Duplicate number/Solution.java
@@ -0,0 +1,23 @@
+
+// Approach: Cycle Detection method                                                                                                  
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int sp = nums[0];
+        int fp = nums[0];
+        do{
+            sp = nums[sp];
+            fp = nums[nums[fp]];
+        }while( sp != fp);
+        
+        fp = nums[0];
+        
+        
+        
+        while(sp != fp ){
+            sp = nums[sp];
+            fp = nums[fp];
+        }
+        return sp;
+    }
+}
