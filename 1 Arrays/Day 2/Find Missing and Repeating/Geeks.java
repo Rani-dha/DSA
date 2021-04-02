7185763bd12d54e92950d710a20fd01af829d077
Dharani T <gct.it18.dharani@gmail.com>
2021-04-02 21:54:41 +0530

    Solution

--- 1 Arrays/Day 2/Find Missing and Repeating/Geeks.java
+++ 1 Arrays/Day 2/Find Missing and Repeating/Geeks.java
@@ -50,7 +50,21 @@ class Solve {
 
         /**
          * Optimal Solution
-         * 
+        
+        int[] ans = new int[2];
+        int i;
+        for (i = 0; i < n; i++) {
+            int abs_val = Math.abs(arr[i]);
+            if (arr[abs_val - 1] > 0)
+                arr[abs_val - 1] = -arr[abs_val - 1];
+            else
+                ans[0] = abs_val;
+        }
+        for (i = 0; i < n; i++) {
+            if (arr[i] > 0) ans[1] = i + 1;
+        }
+        return ans;
+
          */
 
     }
