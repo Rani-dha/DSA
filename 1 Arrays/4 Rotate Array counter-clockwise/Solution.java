f6eecdd5c8fe307fe6b8e284cc0f5959b6eb1f11
Dharani T <gct.it18.dharani@gmail.com>
2021-04-03 23:07:31 +0530

    Rotate counter-clockwise

--- 1 Arrays/4 Rotate Array counter-clockwise/Solution.java
+++ 1 Arrays/4 Rotate Array counter-clockwise/Solution.java
@@ -1,4 +1,4 @@
-// Program 4: https://practice.geeksforgeeks.org/problems/rotate-array-by-n-elements-1587115621/1/
+// Program 4:https://practice.geeksforgeeks.org/problems/rotate-array-by-n-elements-1587115621/1/
 
 class Solution
 {
@@ -6,21 +6,20 @@ class Solution
     static void rotateArr(int arr[], int d, int n)
     {
         
-        //Approach 1:
-        
-        // 1) Store the first d elements in a temporary array.
-        // 2) Shift rest of the elements of arr[] towards left.
-        // 3) Store back the d elements in the same order.
-
+        //Approach 1:     
 
         d = d%n; //If the rotation is greater than size of the array, it's enough to rotate for d%n times.
         int temp[] = new int[d];
+
+        // 1) Store the first d elements in a temporary array.
         for(int i=0; i<d;i++)
             temp[i] = arr[i];
         
+        // 2) Shift rest of the elements of arr[] towards left.
         for(int i=0; i< n-d; i++)
             arr[i] = arr[i+d];
             
+        // 3) Store back the d elements in the same order.
         for(int i=n-d,j=0; i<n; i++,j++) 
               arr[i] = temp[j];      
             
@@ -30,7 +29,6 @@ class Solution
 
 /**
     // Approach 2 reversal of array.
-
     // 1) Reverse first d elements in the array starting from first index.
     // 2) Reverse the next (n-d) elemets left in the array.
     // 3) Reverse the whole array. 
