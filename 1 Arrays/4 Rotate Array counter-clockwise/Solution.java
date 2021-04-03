a9d9027ab9a5f9ee2e7fe6718200efc530875ee7
Dharani T <gct.it18.dharani@gmail.com>
2021-04-03 23:02:15 +0530

    Approach 2 Reversal of array

--- 1 Arrays/4 Rotate Array counter-clockwise/Solution.java
+++ 1 Arrays/4 Rotate Array counter-clockwise/Solution.java
@@ -22,7 +22,50 @@ class Solution
             arr[i] = arr[i+d];
             
         for(int i=n-d,j=0; i<n; i++,j++) 
-              arr[i] = temp[j];
+              arr[i] = temp[j];      
             
     }
 }
+
+
+/**
+    // Approach 2 reversal of array.
+
+    // 1) Reverse first d elements in the array starting from first index.
+    // 2) Reverse the next (n-d) elemets left in the array.
+    // 3) Reverse the whole array. 
+
+
+class Solution
+{
+    //Function to reverse any part of the array.
+    static void rvereseArray(int arr[], int start, int end) 
+    { 
+        int temp; 
+
+        while (start < end) { 
+            //Swapping values at start index and end index.
+            temp = arr[start]; 
+            arr[start] = arr[end]; 
+            arr[end] = temp;
+            //Updating the values of start and end.
+            start++; 
+            end--; 
+        } 
+    }
+    //Function to rotate an array by d elements in counter-clockwise direction. 
+    static void rotateArr(int arr[], int d, int n)
+    {
+        //First reversing d elements from starting index.
+        rvereseArray(arr, 0, d-1);
+        //Then reversing the last n-d elements.
+        rvereseArray(arr, d, n - 1); 
+        //Finally, reversing the whole array.
+        rvereseArray(arr, 0, n - 1); 
+    }
+}
+ 
+ */
+
+
+
