057dcb080cfcbf2ec6488423f7ccb28a50059309
Dharani T <gct.it18.dharani@gmail.com>
2021-04-04 21:07:08 +0530

    kadane's algo

---
+++ 1 Arrays/6 Kadane's Algorithm/Solution.java
@@ -0,0 +1,30 @@
+// Program 6: 
+
+
+class Solution{
+    //Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(int arr[], int n){
+        
+        int maxEndHere = 0, maxSoFar = Integer.MIN_VALUE;
+        
+        //Iterating over the array.
+	    for(int i=0; i<n; i++){
+	        
+	        //Updating max sum till current index.
+	        maxEndHere+=arr[i];
+	        //Storing max sum so far by choosing maximum between max 
+            //sum so far and max sum till current index.
+	        if(maxSoFar < maxEndHere)
+                 maxSoFar = maxEndHere;
+	        
+	        //If max sum till current index is negative, we do not need to add
+            //it to result so we update it to zero.
+	        if(maxEndHere<0)
+	            maxEndHere=0;
+
+        }
+         //returning the result.   
+        return maxSoFar;
+    }
+}
+
