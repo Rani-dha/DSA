4f8edd62674bb535bba61be4f7a1de1009b9f7a0
Dharani T <gct.it18.dharani@gmail.com>
2021-04-12 22:37:11 +0530

    Integer to Roman

---
+++ 3 Strings/Integer to Roman/GFG.java
@@ -0,0 +1,23 @@
+// Program 3: Integer to Roman
+// 
+
+    class GfG
+{
+	String convertToRoman(int n)
+	{
+		String strrom[][] = {{"","I","II","III","IV","V","VI","VII","VIII","IX"},
+							   {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"},
+							   {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"},
+							   {"","M","MM","MMM","","","","","",""}};
+		int i = 0;
+		String str = "";
+		while(n>0)
+		{
+			str = strrom[i][n%10]+str;
+			n = n / 10;
+			i++;
+		}
+		return str;
+	}
+	
+}
