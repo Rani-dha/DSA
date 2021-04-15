c74f85987d35043f66f1e87cb721e538fbc5b7c9
Dharani T <gct.it18.dharani@gmail.com>
2021-04-15 22:35:14 +0530

    Implementation of STRSTR

---
+++ 3 Strings/5 Implementation of STRSTR/GfG.java
@@ -0,0 +1,13 @@
+// Program 5: Implementation of STRSTR
+// https://practice.geeksforgeeks.org/problems/implement-strstr/1
+
+/**
+ * 
+ * A simple solution is to one by one check every index of s1. For every index,
+ * check if s2 is present. O(n*m)
+ * 
+ * For the efficient solution, we can use KMP algorithm Time complexity O(n+m) Z
+ * algorithm Rabin-Karp Algorithm
+ * 
+ * 
+ */
