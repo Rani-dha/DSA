acda2ba8457e2a1e15baf04dc990c77a23d6a387
Dharani T <gct.it18.dharani@gmail.com>
2021-04-16 21:40:44 +0530

    count and say

---
+++ 3 Strings/8 Count and Say/Solution.java
@@ -0,0 +1,24 @@
+//
+//
+
+class Solution {
+    public String countAndSay(int n) {
+       if(n <= 0)
+           return "";
+        String result = "1";
+        while( n>1){
+            StringBuilder current = new StringBuilder();
+            for(int i=0; i<result.length(); i++){
+                int count =1;
+                while( i+1 < result.length() && result.charAt(i) == result.charAt(i+1)){
+                    ++count;
+                    ++i;
+                }
+                current.append(count).append(result.charAt(i));
+            }
+            result = current.toString();
+            --n;
+        }
+        return result;
+    }
+}
