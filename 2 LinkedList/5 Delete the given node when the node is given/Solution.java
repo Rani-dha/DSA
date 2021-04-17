03598de5828290419fd58e73eb945e584190fd22
Dharani T <gct.it18.dharani@gmail.com>
2021-04-17 22:59:03 +0530

    Delete the given node

--- 2 LinkedList/5 Delete the given node when the node is given/Solution.java
+++ 2 LinkedList/5 Delete the given node when the node is given/Solution.java
@@ -1,5 +1,5 @@
-//
-//
+// Program 5: Delete the given node when the node is given O(1) solution
+//  https://leetcode.com/problems/delete-node-in-a-linked-list/submissions/
 
 /**
  * Definition for singly-linked list.
@@ -12,6 +12,6 @@
 class Solution {
     public void deleteNode(ListNode node) {
         node.val = node.next.val; // Copying the next's node value to the current node value.
-        node.next = node.next.next;  // Point the node's next to  node's next next, bywhich you can remove the connection.           
+        node.next = node.next.next;  // Point the node's next to  node's next next, bywhich you can remove the connection between node and node next.         
     }
 }
