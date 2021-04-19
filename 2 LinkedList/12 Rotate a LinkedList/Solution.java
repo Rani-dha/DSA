5d75b3bcc267128919d43af5c178a33443e07810
Dharani T <gct.it18.dharani@gmail.com>
2021-04-19 22:47:33 +0530

    Rotate a linkedlist

---
+++ 2 LinkedList/12 Rotate a LinkedList/Solution.java
@@ -0,0 +1,39 @@
+// Program 12:
+//
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        
+        if(head == null || head.next == null || k == 0) // edge cases
+            return head;
+        
+        int length = 1; // Must be 1
+        
+        ListNode temp =  head;
+        while(temp.next != null){
+            length++;
+            temp =temp.next;
+        }
+        
+        temp.next = head;
+        k = length - ( k%length);
+        
+        while( k-- != 0)
+            temp = temp.next;
+        
+        head = temp.next;
+        temp.next = null;
+                
+        return head;
+    }
+}
