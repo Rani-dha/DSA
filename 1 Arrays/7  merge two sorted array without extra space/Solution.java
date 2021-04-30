f22a21d30dae703fd2831b4b8a7e419b1b23eb59
Dharani T <gct.it18.dharani@gmail.com>
2021-04-30 23:44:59 +0530

    merge without extra space

---
+++ 1 Arrays/7  merge two sorted array without extra space/Solution.java
@@ -0,0 +1,34 @@
+//
+//
+
+class Solution {
+
+    public void merge(int arr1[], int arr2[], int n, int m) {
+        // code here
+        
+         // code here
+        int gap =  (n+m)/2 +  (n+m)%2 ;
+        while( gap > 1){
+            int i,j,k;
+            for( i = 0; i+gap < n; i++){
+                if( arr1[i] > arr1[i+gap]){
+                    arr1[i] += arr1[i+gap] - ( arr1[i+gap] = arr1[i]);
+                }
+            }
+            
+            for( j=0; i<n && j <m; i++,j++){
+                    if( arr1[i] > arr2[j]){
+                    arr1[i] += arr2[j] - ( arr2[j] = arr1[i]);
+                }
+            }
+            
+            for( k=0; (k+gap) < m; k++ ){
+                if( arr2[k] > arr2[k+gap]){
+                    arr2[k] += arr2[k+gap] - ( arr2[k+gap] = arr2[k]);
+                } 
+            }
+            
+            gap = (gap/2) + (gap%2);
+        }
+    }
+}
