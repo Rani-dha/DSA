a4524f356f5e991f3310b9a9c97d66934b3a78f0
Dharani T <gct.it18.dharani@gmail.com>
2021-05-01 21:55:26 +0530

    merge without extra space

--- 1 Arrays/7  merge two sorted array without extra space/Solution.java
+++ 1 Arrays/7  merge two sorted array without extra space/Solution.java
@@ -3,32 +3,39 @@
 
 class Solution {
 
+    public int gapNext(int gap) {
+        if (gap <= 1)
+            return 0;
+        return (gap / 2) + (gap % 2);
+    }
+
     public void merge(int arr1[], int arr2[], int n, int m) {
         // code here
-        
-         // code here
-        int gap =  (n+m)/2 +  (n+m)%2 ;
-        while( gap > 1){
-            int i,j,k;
-            for( i = 0; i+gap < n; i++){
-                if( arr1[i] > arr1[i+gap]){
-                    arr1[i] += arr1[i+gap] - ( arr1[i+gap] = arr1[i]);
+        int gap = (n + m);
+        int i, j;
+        // for(gap = gapNext(gap); gap > 0; gap = gapNext(gap)){ // both for and while works fine
+       
+        while ((gap = gapNext(gap)) > 0) {
+            for (i = 0; i + gap < n; i++) {
+                if (arr1[i] > arr1[i + gap]) {
+                    arr1[i] += arr1[i + gap] - (arr1[i + gap] = arr1[i]); // swapping in single line
                 }
             }
-            
-            for( j=0; i<n && j <m; i++,j++){
-                    if( arr1[i] > arr2[j]){
-                    arr1[i] += arr2[j] - ( arr2[j] = arr1[i]);
+
+            for (j = (gap > n) ? gap - n : 0; i < n && j < m; i++, j++) {
+                if (arr1[i] > arr2[j]) {
+                    arr1[i] += arr2[j] - (arr2[j] = arr1[i]);  // swapping in single line
                 }
             }
-            
-            for( k=0; (k+gap) < m; k++ ){
-                if( arr2[k] > arr2[k+gap]){
-                    arr2[k] += arr2[k+gap] - ( arr2[k+gap] = arr2[k]);
-                } 
+
+            if (j < m) {
+                for (j = 0; j + gap < m; j++) {
+                    if (arr2[j] > arr2[j + gap]) {
+                        arr2[j] += arr2[j + gap] - (arr2[j + gap] = arr2[j]);  // swapping in single line
+                    }
+                }
             }
-            
-            gap = (gap/2) + (gap%2);
+
         }
     }
 }
