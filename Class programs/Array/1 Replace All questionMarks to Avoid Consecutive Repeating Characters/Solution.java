8d192c4912fb6ada2bee9fc344c9c0ed34211d92
Dharani T <gct.it18.dharani@gmail.com>
2021-05-13 21:16:01 +0530

    ReplaceAll questionmarks

--- Class programs/Array/1 Replace All questionMarks to Avoid Consecutive Repeating Characters/Solution.java
+++ Class programs/Array/1 Replace All questionMarks to Avoid Consecutive Repeating Characters/Solution.java
@@ -1,5 +1,5 @@
-//
-//
+// Program 1: 1576. Replace All ?'s to Avoid Consecutive Repeating Characters
+// https://leetcode.com/problems/replace-all-s-to-avoid-consecutive-repeating-characters/submissions/
 
 class Solution {
     public String modifyString(String s) {
