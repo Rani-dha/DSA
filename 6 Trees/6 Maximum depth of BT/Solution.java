e2339157aa60ff867a5293ad75a8ee929beb9d5f
Dharani T <gct.it18.dharani@gmail.com>
2021-05-29 21:19:48 +0530

    BT maximum depth

---
+++ 6 Trees/6 Maximum depth of BT/Solution.java
@@ -0,0 +1,31 @@
+// Program 6: Maximum depth of the BT
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/submissions/
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        
+        if( root == null) return 0;
+        
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 3
+
