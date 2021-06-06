339f576afac69ed7bb83f7bb981f77b89959c4ea
Dharani T <gct.it18.dharani@gmail.com>
2021-06-06 22:48:01 +0530

    LL

---
+++ GCT Coding contest/1 Arrays and LinkedList/mergeTwoSortedLL.java
@@ -0,0 +1,41 @@
+// Merge two sorted linked list
+// https://www.hackerrank.com/contests/gct-c/challenges/merge-two-sorted-linked-lists/submissions/code/1334067610
+
+    // Complete the mergeLists function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode mergeLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        if( head1 == null) return head2;
+        if( head2 == null) return head1;
+        
+        if(head1.data > head2.data){
+            SinglyLinkedListNode  temp = head1;
+            head1 = head2;
+            head2 = temp;
+        }
+        SinglyLinkedListNode result = head1;
+        while( head1 != null && head2 != null){
+            SinglyLinkedListNode prevNode = null;
+            while ( head1 != null && head1.data <= head2.data){
+                prevNode = head1;
+                head1 = head1.next;
+            }
+            
+            prevNode.next = head2 ;
+            
+            SinglyLinkedListNode swap = head1;
+            head1 = head2;
+            head2 = swap;           
+            
+        }
+        return result;
+
+    }
