88dfa1ae37da7c72f80a2fcd84abab4f31ce4a86
Dharani T <gct.it18.dharani@gmail.com>
2021-06-08 22:00:22 +0530

    validate ip

--- Class programs/Array/3 Validate IPv4 and Ipv6 address/Solution.java
+++ Class programs/Array/3 Validate IPv4 and Ipv6 address/Solution.java
@@ -33,3 +33,14 @@ class Solution {
         
     }
 }
+
+// Example 1:
+
+// Input: IP = "172.16.254.1"
+// Output: "IPv4"
+// Explanation: This is a valid IPv4 address, return "IPv4".
+// Example 2:
+
+// Input: IP = "2001:0db8:85a3:0:0:8A2E:0370:7334"
+// Output: "IPv6"
+// Explanation: This is a valid IPv6 address, return "IPv6".
