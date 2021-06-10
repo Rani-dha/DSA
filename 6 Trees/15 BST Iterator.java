0d01b71d59781b170a8c1b1cde01d4dd62eda85c
Dharani T <gct.it18.dharani@gmail.com>
2021-06-10 21:08:49 +0530

    bst

--- 6 Trees/15 BST Iterator.java
+++ 6 Trees/15 BST Iterator.java
@@ -1,5 +1,6 @@
 // BST iterator
 // https://leetcode.com/problems/binary-search-tree-iterator/
+// Implemention of the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
 
 /**
  * Definition for a binary tree node.
@@ -47,3 +48,22 @@ class BSTIterator {
  * int param_1 = obj.next();
  * boolean param_2 = obj.hasNext();
  */
+
+
+// Input
+// ["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+// [[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+// Output
+// [null, 3, 7, true, 9, true, 15, true, 20, false]
+
+// Explanation
+// BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+// bSTIterator.next();    // return 3
+// bSTIterator.next();    // return 7
+// bSTIterator.hasNext(); // return True
+// bSTIterator.next();    // return 9
+// bSTIterator.hasNext(); // return True
+// bSTIterator.next();    // return 15
+// bSTIterator.hasNext(); // return True
+// bSTIterator.next();    // return 20
+// bSTIterator.hasNext(); // return False
