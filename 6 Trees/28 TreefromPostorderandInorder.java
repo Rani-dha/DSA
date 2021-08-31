ba4f404b137c0abb891a5584f39a7f2f7ef5fa6e
DHARANI THANGAVEL <gct.it18.dharani@gmail.com>
2021-08-31 22:24:41 +0530

    Tree from Postorder and Inorder

---
+++ 6 Trees/28 TreefromPostorderandInorder.java
@@ -0,0 +1,91 @@
+// Tree from Postorder and Inorder
+// https://practice.geeksforgeeks.org/problems/tree-from-postorder-and-inorder/1#
+
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int value) {
+        data = value;
+        left = null;
+        right = null;
+    }
+}
+
+class InorderPostorderToTree {
+    public void preOrder(Node root) {
+        if (root == null) return;
+
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+
+        preOrder(root.right);
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        InorderPostorderToTree ip = new InorderPostorderToTree();
+        int T = sc.nextInt();
+        while (T > 0) {
+            int n = sc.nextInt();
+            int inorder[] = new int[n];
+            int postorder[] = new int[n];
+            for (int i = 0; i < n; i++) inorder[i] = sc.nextInt();
+            for (int i = 0; i < n; i++) postorder[i] = sc.nextInt();
+            GfG g = new GfG();
+            Node root = g.buildTree(inorder, postorder, n);
+            ip.preOrder(root);
+            System.out.println();
+
+            T--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/* Tree node structure
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+
+        Node(int value)
+    {
+        data = value;
+        left = null;
+        right = null;
+    }
+}*/
+
+
+class GfG
+{
+    //Function to return a tree created from postorder and inoreder traversals.
+    Node buildTree(int inorder[], int postorder[], int n) {
+        // Your code here
+        if (inorder == null || postorder == null || inorder.length != postorder.length)
+            return null;
+        HashMap<Integer, Integer> hm = new HashMap<Integer,Integer>();
+        for (int i=0;i<inorder.length;++i)
+            hm.put(inorder[i], i);
+        return buildTreePostIn(inorder, 0, inorder.length-1, postorder, 0, 
+                              postorder.length-1,hm);
+    }
+
+    private  Node buildTreePostIn(int[] inorder, int is, int ie, int[] postorder, int ps, int pe, 
+                                     HashMap<Integer,Integer> hm){
+        if (ps>pe || is>ie) return null;
+        Node root = new Node(postorder[pe]);
+        int ri = hm.get(postorder[pe]);
+        Node leftchild = buildTreePostIn(inorder, is, ri-1, postorder, ps, ps+ri-is-1, hm);
+        Node rightchild = buildTreePostIn(inorder,ri+1, ie, postorder, ps+ri-is, pe-1, hm);
+        root.left = leftchild;
+        root.right = rightchild;
+        return root;
+    }
+}
